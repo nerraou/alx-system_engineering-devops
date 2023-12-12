858619615a30de91f00b77703a60098e13e59c6c
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-12-12 23:41:02 +0100

    recursive

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Recurse it!
+"""
+
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    res = get(
+          "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+          headers={"User-Agent": "nerraou"},
+          params={"after": after},
+                )
+    if res.status_code == 200:
+        for data in res.json().get("data").get("children"):
+            values = data.get("data")
+            title = values.get("title")
+            hot_list.append(title)
+        after = res.json().get("data").get("after")
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
+    else:
+        print(None)
