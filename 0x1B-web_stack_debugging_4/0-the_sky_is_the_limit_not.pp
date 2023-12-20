0f9cff48e7f4dcf3680c402423be40bcd31df1dd
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-12-20 04:29:52 -0800

    fix(nginx): max open files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increase max nginx max open files limit
+
+# edit ULMIT
+exec { 'nginx set max open files':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+exec { 'restart nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
